dbeff02bf7479788c0a29d6c775fbe94c3ded04f
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-06 00:26:50 -0600

    completed day 3 part 1

--- Day3/3.1.py
+++ Day3/3.1.py
@@ -1,7 +1,16 @@
-def open_txt_file_and_read_lines(txt_file):
+def parse_indexed_values_from_txt_file(txt_file):
+    index_value = -3
+    new_string = ''
     with open(txt_file) as f:
         for line in f:
-            print(line)
+            pattern_data = line.replace('\n', '')
+            index_value = index_value + 3
+            while index_value > 30:
+                index_value = index_value - len(pattern_data)
+                if index_value <= 30:
+                    break
+            new_string += pattern_data[index_value]
+    print(new_string.count('#'))
 
 
-open_txt_file_and_read_lines('pattern.txt')
+parse_indexed_values_from_txt_file('pattern.txt')

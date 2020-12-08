a094cbd15c4d5901040dcffaaaa69779cb06521c
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-08 17:04:01 -0600

    completed Day 5 part 1 and 2. added csv of output data for solution

--- Day5/d5.py
+++ Day5/d5.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def ingest_boarding_pass_file_and_store_as_list(boarding_passes):
     boarding_pass_list = []
     with open(boarding_passes, 'r') as f:
@@ -60,6 +62,40 @@ def front_back_rows(boarding_pass_list):
     return boarding_passes_dict
 
 
+def left_right_rows(boarding_pass_list):
+    boarding_passes_column_dict = {}
+    column_range = list(range(0, 8))
+    half_column_range = len(column_range)//2
+    for boarding_pass in boarding_pass_list:
+        boarding_pass_column_dict = {}
+        if boarding_pass[7] == 'R':
+            column_range_1 = column_range[half_column_range:]
+            half_column_range_1 = len(column_range_1)//2
+        if boarding_pass[7] == 'L':
+            column_range_1 = column_range[:half_column_range]
+            half_column_range_1 = len(column_range_1)//2
+        if boarding_pass[8] == 'R':
+            column_range_2 = column_range_1[half_column_range_1:]
+            half_column_range_2 = len(column_range_2)//2
+        if boarding_pass[8] == 'L':
+            column_range_2 = column_range_1[:half_column_range_1]
+            half_column_range_2 = len(column_range_2)//2
+        if boarding_pass[9] == 'R':
+            column_range_3 = column_range_2[half_column_range_2:]
+            half_column_range_3 = len(column_range_3)//2
+        if boarding_pass[9] == 'L':
+            column_range_3 = column_range_2[:half_column_range_2]
+            half_column_range_3 = len(column_range_3)//2
+        boarding_pass_column_dict['column'] = column_range_3[0]
+        boarding_passes_column_dict[boarding_pass] = boarding_pass_column_dict
+    return boarding_passes_column_dict
+
+
 boarding_pass_list = (ingest_boarding_pass_file_and_store_as_list
 ('boarding_passes.txt'))
-print(front_back_rows(boarding_pass_list))
+boarding_passes_row_dict = front_back_rows(boarding_pass_list)
+row_df = pd.DataFrame(boarding_passes_row_dict)
+row_df.to_csv('output_files/rows.csv')
+boarding_passes_column_dict = left_right_rows(boarding_pass_list)
+column_df = pd.DataFrame(boarding_passes_column_dict)
+column_df.to_csv('output_files/columns.csv')
